9be8dca035f4fd0905ead0009bdb004dbd9fd313
victoriatobio <vtobio@udesa.edu.ar>
2025-09-10 12:19:22 -0300

    (formulario de búsqueda (sin terminar))

--- proyecto/proyecto-integrador/src/components/Header/Header.js
+++ proyecto/proyecto-integrador/src/components/Header/Header.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 import "./HeaderStyles.css";
 
-function Header() {
-    return(
-        <React.Fragment>
-        <header className="header">
-            <div className="logo-container">
-                <img src="./img/logo.jpg" alt="AfterCredits" className="logo" />
-                <p className="title">After Credits</p>
-            </div>
-        </header>
-        </React.Fragment>
-    )
+class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = { termino : " "}; 
+    }
+
+    evitarSubmit(event){
+        event.preventDefault();
+        if (this.state.termino !== " "){
+            window.location.href = "/buscar?query=" + this.state.termino;
+        }
+    }
+
+    controlarCambios(event){
+        this.setState({ termino : event.target.value });
+    }
+
+    render(){
+        return (
+            <header className="header">
+                <div className="logo-container">
+                    <img src="./img/logo.jpg" alt="AfterCredits" className="logo" />
+                    <p className="title"> After Credits </p>
+                </div>
+
+                <form onSubmit = {(event) => this.evitarSubmit(event)}>
+                    <label> Buscar: </label>
+                    <input type="text" onChange={(event) => this.controlarCambios(event)} value="{this.state.termino}" />
+                </form>
+            </header>
+        )
+    }
 }
 
 export default Header;
