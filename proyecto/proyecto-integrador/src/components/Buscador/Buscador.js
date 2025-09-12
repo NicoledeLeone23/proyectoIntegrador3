8c1a92b5731dbb7dfa10658c756e0bfe625cad91
vickycastanio <vcastanoseippel@udesa.edu.ar>
2025-09-12 09:10:37 -0300

    mover y correcion del buscador

---
+++ proyecto/proyecto-integrador/src/components/Buscador/Buscador.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom"; // v5
+
+class Buscador extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+  }
+
+  controlarForm = (e) => {
+    e.preventDefault();
+    this.props.history.push('/results/' + this.state.input);
+  };
+
+  controlarInput = (e) => {
+    this.setState({ input: e.target.value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.controlarForm} className="buscador-form">
+        <input
+          placeholder="Buscador"
+          value={this.state.input}
+          onChange={this.controlarInput}
+          className="buscador-input"
+        />
+        <button type="submit" className="buscador-boton">Buscar</button>
+      </form>
+    );
+  }
+}
+
+export default withRouter(Buscador);
